c4eb4eab229ea7c4279bfab09d282727ed14d306
Chetna Pant <chetnapant91@gmail.com>
2025-12-04 22:42:19 +0530

    theme module uploaded

---
+++ ThemeToggle.jsx
@@ -0,0 +1,17 @@
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
+import { useGlobalContext } from './Context';
+
+
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleTheme } = useGlobalContext();
+
+  return (
+    <section className='toggle-container'>
+      <button className='dark-toggle' onClick={toggleTheme}>
+         {isDarkTheme? <BsFillMoonFill className='toggle-icon'/>:<BsFillSunFill className='toggle-icon'/>}
+      </button>
+    </section>
+  );
+}
+
+export default ThemeToggle
